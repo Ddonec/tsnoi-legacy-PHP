83dd343a12128ce8e9811615d7a972a86b5f9faf
ddonec <rolldonec@gmail.com>
2024-07-29 15:49:25 +0300

    feat: way template get comtent

--- tsnoi/page-way-template.php
+++ tsnoi/page-way-template.php
@@ -9,6 +9,169 @@ get_header();
 
 
 
+<main>
+      <section class="first-section-way">
+        <div class="default-block-blue-bg default-block-blue-bg-way way-template-banner">
+          <img
+            src="./assets/content/way-template-absolute-1.png"
+            class="fly-element second-fly-element-way"
+          />
+          <img
+            src="./assets/content/way-template-absolute-2.png"
+            class="fly-element third-fly-element-way"
+          />
+          <div class="first-block-blue-bg-overflow">
+            <img
+              src="./assets/content/way-template-absolute-bg.png"
+              class="first-fly-element-way"
+            />
+            <div class="blue-bg-main-text blue-bg-main-text-way">
+              <div class="blue-bg-main__button">Направление</div>
+              <h1 class="h1-text">Наши мероприятия <br />в Санкт-Петербурге</h1>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="section-standart-100 way-template-s">
+        <div class="white-way-block">
+          <img
+            class="white-way-block__image"
+            src="./assets/content/way-photo-1.png"
+            alt=""
+          />
+          <div class="white-way-block__info">
+            <h2 class="white-way-block__title">Невская Ассамблея</h2>
+            <p class="white-way-block__subtitle">
+              Значимость этих проблем настолько очевидна, что базовый вектор
+              развития позволяет оценить значение экспериментов, поражающих по
+              своей масштабности и грандиозности.
+              <br /><br />
+              Лишь представители современных социальных резервов объединены в
+              целые кластеры себе подобных.
+            </p>
+            <a href="">
+              <div class="white-way-block__button">
+                Перейти на сайт
+                <svg
+                  width="25"
+                  height="25"
+                  viewBox="0 0 25 25"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M5.5 12.9216L19.5 12.9216"
+                    stroke="white"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M13.5 6.92163L19.5 12.9216L13.5 18.9216"
+                    stroke="white"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg></div
+            ></a>
+          </div>
+        </div>
+        <div class="white-way-block">
+          <div class="white-way-block__info">
+            <h2 class="white-way-block__title">
+              Фестиваль дошкольного образования
+            </h2>
+            <p class="white-way-block__subtitle">
+              Значимость этих проблем настолько очевидна, что базовый вектор
+              развития позволяет оценить значение экспериментов, поражающих по
+              своей масштабности и грандиозности.
+              <br /><br />
+              Лишь представители современных социальных резервов объединены в
+              целые кластеры себе подобных.
+            </p>
+            <a href=""
+              ><div class="white-way-block__button">
+                Перейти на сайт
+                <svg
+                  width="25"
+                  height="25"
+                  viewBox="0 0 25 25"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M5.5 12.9216L19.5 12.9216"
+                    stroke="white"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M13.5 6.92163L19.5 12.9216L13.5 18.9216"
+                    stroke="white"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg></div
+            ></a>
+          </div>
+          <img
+            class="white-way-block__image"
+            src="./assets/content/way-photo-2.png"
+            alt=""
+          />
+        </div>
+        <div class="white-way-block">
+          <img
+            class="white-way-block__image"
+            src="./assets/content/way-photo-1.png"
+            alt=""
+          />
+          <div class="white-way-block__info">
+            <h2 class="white-way-block__title">
+              Педагогический съезд «Моя страна»
+            </h2>
+            <p class="white-way-block__subtitle">
+              Значимость этих проблем настолько очевидна, что базовый вектор
+              развития позволяет оценить значение экспериментов, поражающих по
+              своей масштабности и грандиозности.
+              <br /><br />
+              Лишь представители современных социальных резервов объединены в
+              целые кластеры себе подобных.
+            </p>
+            <a href=""
+              ><div class="white-way-block__button">
+                Перейти на сайт
+                <svg
+                  width="25"
+                  height="25"
+                  viewBox="0 0 25 25"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M5.5 12.9216L19.5 12.9216"
+                    stroke="white"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M13.5 6.92163L19.5 12.9216L13.5 18.9216"
+                    stroke="white"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg></div
+            ></a>
+          </div>
+        </div>
+      </section>
+    </main>
+
 
 
 <?php
