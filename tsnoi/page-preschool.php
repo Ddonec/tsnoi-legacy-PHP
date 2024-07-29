39e93b60da6d768cbe6be24cd73784e6160f5b91
ddonec <rolldonec@gmail.com>
2024-07-29 15:37:45 +0300

    feat: fos preschool complete

--- tsnoi/page-preschool.php
+++ tsnoi/page-preschool.php
@@ -158,22 +158,21 @@ get_header();
                 <?php endwhile; ?>
             <?php endif; ?>
         </div>
-        <div class="view-all-reviews-btn">Все отзывы</div>
+        <div class="view-all-reviews-btn"><?php the_field('all-reviews-btn'); ?></div>
     </section>
 
     <section class="fly-bottom-section-ways-page">
         <div class="blue-bg-container-for-fos">
             <div class="blue-bg-container-left">
                 <h3 class="blue-bg-container__header">
-                    Оставьте заявку, чтобы начать обучение
+                <?php the_field('fos-title'); ?>
                 </h3>
             </div>
             <div class="blue-bg-container-right">
                 <form class="blue-bg-container__fos-box">
-                    <h4 class="fos-box_title">Заполните форму ниже</h4>
+                    <h4 class="fos-box_title"><?php the_field('fos-text'); ?></h4>
                     <p class="fos-box_subtitle">
-                        Оставьте свои данные и менеджер свяжется с вами в ближайшее
-                        время
+                    <?php the_field('fos-subtitle'); ?>
                     </p>
                     <input class="fos-box_name-input" placeholder="Ваше имя" type="text" />
                     <input placeholder="Номер телефона" class="fos-box_number-input" type="number" />
@@ -182,11 +181,11 @@ get_header();
                         <label class="checkbox-agree-container__label" for="agree">
                             <div class="checkbox"></div>
                             <p class="checkbox-agree-container__text">
-                                Я соглашаюсь с политикой конфиденциальности
+                            <?php the_field('fos-agree'); ?>
                             </p>
                         </label>
                     </div>
-                    <button class="fos-box_btn-agree">Записаться</button>
+                    <button class="fos-box_btn-agree"><?php the_field('fos-btn'); ?></button>
                 </form>
                 <div class="messenger-feedback-box">
                     <div class="messenger-feedback-box__icon-box">
@@ -204,8 +203,7 @@ get_header();
                     </div>
 
                     <p class="messenger-feedback-box__text">
-                        Так же для расчета, вы можете связаться в любом удобном для вас
-                        месенджере
+                    <?php the_field('fos-feedback'); ?>
                     </p>
                 </div>
             </div>
